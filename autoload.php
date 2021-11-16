a9c64e6237749aa892e7bd6be501f85cbebab600
Lucas Alcantara Rodrigues Volpati <lcs.rodrigues16@hotmail.com>
2021-11-16 13:18:14 -0300

    Remanejamento de variáveis
    Create $separator variable.

--- autoload.php
+++ autoload.php
@@ -5,13 +5,14 @@
 
 function autoload($classe) { //A váriavel $classe recebe o nome da classe que está sendo chamada.
 
+	$separator = DIRECTORY_SEPARATOR; //Barra ou Contra Barra (linux ou windows)
 
-	$dirBase = __DIR__ . DIRECTORY_SEPARATOR; //Diretório base do projeto (pasta raíz) deve ser algo do tipo /var/www/html/projeto (linux) ou C:\\xampp\htdocs\projeto.
+	$dirBase = __DIR__ . $separator; //Diretório base do projeto (pasta raíz) deve ser algo do tipo /var/www/html/projeto (linux) ou C:\\xampp\htdocs\projeto.
 	
 	//var_dump($dirBase);exit; //Comando para teste, mostra o caminho do diretório base.
 
 
-	$classe = $dirBase . 'class' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR , $classe . ".php");
+	$classe = $dirBase . 'class' . $separator . str_replace('\\', $separator, $classe . ".php");
 
 	//var_dump($classe);exit; //Comando para teste, mostra o caminho completo da localização da classe.
 	
